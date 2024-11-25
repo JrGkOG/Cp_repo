03655bfe5830b5f3b9b8009e50d92b4358cf2f48
adofm <karthik.maniam008@gmail.com>
2024-11-25 18:37:42 +0530

    finished CF344-D2-A

--- CF344-D2-A.cpp
+++ CF344-D2-A.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    queue<int>q;
-    q.push(1);
-    cout<<q.front();
+    int n;
+    cin>>n;
+    string prev="-1";
+    int blocks=0;
+    for(int i=0;i<n;i++){
+        string number;
+        cin>>number;
+        if(prev=="-1"){
+            prev=number;
+            blocks+=1;
+        }else{
+            if(number=="10" && prev=="01"){
+                blocks+=1;
+                prev=number;
+            }
+            else if(number=="01" && prev=="10"){
+                blocks+=1;
+                prev=number;
+            }
+            else{
+                prev=number;
+            }
+        }
+    }
+    cout<<blocks;
+    return 0;
 }
