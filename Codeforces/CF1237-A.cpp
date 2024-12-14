50efd4fff8879f6d53eb5b64f5eeaf3046998375
adofm <karthik.maniam008@gmail.com>
2024-12-14 09:36:55 +0530

    added the things related to todays's work

--- Codeforces/CF1237-A.cpp
+++ Codeforces/CF1237-A.cpp
@@ -1,9 +1,48 @@
-#include<bits/stdc++.h>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int inputs;
+    cin >> inputs;
 
+    bool flag = false; // Alternate flag for odd number handling
+    vector<int> ans(inputs);
+    int total_sum = 0;
+
+    // First pass: Transform numbers
+    for (int i = 0; i < inputs; i++) {
+        int num;
+        cin >> num;
+
+        if (num % 2 == 0) { // Even number
+            ans[i] = num / 2;
+        } else { // Odd number
+            if (flag) {
+                ans[i] = (num > 0 ? (num / 2) + 1 : (num / 2) - 1);
+            } else {
+                ans[i] = num / 2;
+            }
+            flag = !flag; // Toggle flag
+        }
+        total_sum += ans[i];
+    }
+
+    // Second pass: Adjust to ensure sum is zero
+    if (total_sum != 0) {
+        for (int i = 0; i < inputs; i++) {
+            if (ans[i] != 0 && abs(total_sum) == 1) { 
+                // Adjust one odd number slightly
+                ans[i] -= total_sum;
+                total_sum = 0;
+                break;
+            }
+        }
+    }
+
+    // Output the results
+    for (int i = 0; i < inputs; i++) {
+        cout << ans[i] << "\n";
+    }
+    cout << "Sum: " << total_sum << "\n";
     return 0;
 }
