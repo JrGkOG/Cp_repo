8904f9b122114cbdbf50ac543509d7d99dc84129
adofm <karthik.maniam008@gmail.com>
2024-12-20 13:57:47 +0530

    added the things

--- Swami Saranam/Codeforces/contest/993/d.cpp
+++ Swami Saranam/Codeforces/contest/993/d.cpp
@@ -2,30 +2,39 @@
 using namespace std;
 #define ll long long 
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int t2;
-        cin>>t2;
-        vector<int>there(t2);
-        vector<int>notThere(t2);
-        iota(notThere.begin(),notThere.end(),1);
-        for(int i=0;i<t2;i++){
-            cin>>there[i];
-            notThere.erase(notThere.begin()+there[i]-1);
+        cin >> t2;
+        set<int> st;
+        vector<int> nums(t2);
+        set<int> notThere;
+        for (int i = 1; i <= t2; i++) {
+            notThere.insert(i);
         }
-        vector<int>ans(t2,-1);
-        for(int i=0;i<t2;i++){
-            if(ans[i]==there[i]){
-                ans[i]=notThere[i];
-                cout<<ans[i]<<" ";
-            }
-            else{
-                ans[i]=there[i];
-                cout<<ans[i]<<" ";
+        for (int i = 0; i < t2; i++) {
+            cin >> nums[i];
+            st.insert(nums[i]);
+            if (notThere.find(nums[i]) != notThere.end()) {
+                notThere.erase(nums[i]);
             }
         }
+        vector<int> ans;
+        for (int i = 0; i < t2; i++) {
+            if (st.find(nums[i]) != st.end()) {
+                ans.push_back(nums[i]);
+                st.erase(nums[i]);  
+            } else {
+                ans.push_back(*notThere.begin()); 
+                notThere.erase(notThere.begin()); 
+            }
+        }
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
---
+++ Swami Saranam/Codeforces/contest/993/d2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int t2;
+        cin>>t2;
+        unordered_map<pair<int,bool>>mp;
+        set<int>notThere;
+        vector<int>nums(t2);
+        vector<int>ans(t2);
+        for(int i=0;i<t2;i++){
+            int number;
+            cin>>number;
+            nums[i]=number;
+            ans[i]=number;
+    
+        }
+    }
+    return 0;
+}
