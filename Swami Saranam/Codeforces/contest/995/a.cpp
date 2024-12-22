88f5cc1cf9975597041b85a8660069408a1b657c
adofm <karthik.maniam008@gmail.com>
2024-12-22 19:59:30 +0530

    added the contest codes

---
+++ Swami Saranam/Codeforces/contest/995/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main(){
+    fast();
+    int t;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
