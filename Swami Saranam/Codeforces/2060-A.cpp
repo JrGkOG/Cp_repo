3d420622053334fd4b91e455e4bd357d4291aac2
adofm <karthik.maniam008@gmail.com>
2025-02-25 19:56:52 +0530

    solved more

---
+++ Swami Saranam/Codeforces/2060-A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);   
+
+void func(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a1,a2,a4,a5;
+        cin>>a1>>a2>>a4>>a5;
+        
+    }
+}
+int main(){
+    fast();
+    func();
+    return 0;
+}
