ed4515a940772e62a1489bd812e3ca205b1cf8f9
adofm <karthik.maniam008@gmail.com>
2025-03-18 18:09:33 +0530

    solved contest 172 edu

--- Swami Saranam/Codeforces/contest/edu176/a.cpp
+++ Swami Saranam/Codeforces/contest/edu176/a.cpp
@@ -3,16 +3,36 @@ using namespace std;
 #define ll long long
 #define fast()                        \
     ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);   
+    cin.tie(NULL);
 
-void func(){
+void func()
+{
     int t;
-    cin>>t;
-    while(t--){
-        
+    cin >> t;
+    while (t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        if (n % 2 == 0)
+        {
+            cout<<(n + k - 2) / (k - 1)<<endl;
+        }
+        else
+        {
+            ll x = n - k;
+            if (x <= 0)
+            {
+                cout<<1<<endl;
+            }
+            else{
+                cout<<1 + (x + k - 2) / (k - 1)<<endl;
+            }
+
+        }
     }
 }
-int main(){
+int main()
+{
     fast();
     func();
     return 0;
--- Swami Saranam/Codeforces/contest/edu176/c.cpp
+++ Swami Saranam/Codeforces/contest/edu176/c.cpp
@@ -3,16 +3,37 @@ using namespace std;
 #define ll long long
 #define fast()                        \
     ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);   
+    cin.tie(NULL);
 
-void func(){
+void func()
+{
     int t;
-    cin>>t;
-    while(t--){
-        
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end(), greater<long long>());
+        if(k>=2){
+            long long maxCost = 0;
+            for (int i = 0; i < k; i++)
+            {
+                maxCost += a[i];
+            }
+            maxCost += a[k];
+            cout << maxCost << endl;
+        }
+
     }
 }
-int main(){
+
+int main()
+{
     fast();
     func();
     return 0;
