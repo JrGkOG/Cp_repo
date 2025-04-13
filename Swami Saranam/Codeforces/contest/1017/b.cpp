ef373c167cf6c174f74ea9fa132a6c32e844422e
adofm <karthik.maniam008@gmail.com>
2025-04-13 22:37:38 +0530

    solved one mroe

---
+++ Swami Saranam/Codeforces/contest/1017/b.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#define ll long long 
+
+int main(){
+  printf("hello world how are you \n");
+
+  return 0;
+}
---
+++ Swami Saranam/Codeforces/contest/aprilfools/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);   
+
+void func(){
+    int t;
+    t=1;
+    while(t--){
+        cout<<"safety"<<endl;
+    }
+}
+int main(){
+    fast();
+    func();
+    return 0;
+}
