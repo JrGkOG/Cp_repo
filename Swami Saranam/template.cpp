5ca2bdb0a49dfa70d3e1a3d2af9fa7b4e40027eb
adofm <karthik.maniam008@gmail.com>
2025-06-19 10:00:33 +0530

    changed the template cpp

--- Swami Saranam/template.cpp
+++ Swami Saranam/template.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define yes "Yes"
-#define no "No"
 #define fast()                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);   
-
+#define ll long long
+#define f(i, n) for (ll i = 0; i < n; i++)
+#define ia(a, n) \
+    ll a[n];     \
+    f(i, n) cin >> a[i]
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i, n) cin >> v[i]
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 void func(){
     int t;
     cin>>t;
