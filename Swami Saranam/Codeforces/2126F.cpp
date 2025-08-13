aa51c6ed3c14e987ae06c72259cd72324515479b
adofm <karthik.maniam008@gmail.com>
2025-08-13 20:28:21 +0530

    sovled

--- Swami Saranam/Codeforces/2126F.cpp
+++ Swami Saranam/Codeforces/2126F.cpp
@@ -22,31 +22,7 @@ void dfs(int node, int parent) {
 void solve() {
     ll n, q;
     cin >> n >> q;
-    // Use 1-based indexing for color to match input, avoids off-by-one errors
-    color.assign(n + 1, 0); 
-    for (int i = 1; i <= n; i++) cin >> color[i];
-
-    graph.assign(n + 1, {});
-    for (int i = 0; i < n - 1; i++) {
-        ll src, dest, wt;
-        cin >> src >> dest >> wt;
-        graph[src].push_back({dest, wt});
-        graph[dest].push_back({src, wt});
-    }
-
-    // --- LOGIC FIXED HERE ---
-    for (int i = 0; i < q; i++) {
-        ll v, new_c;
-        cin >> v >> new_c;
-        color[v-1] = new_c; // Repaint the single node for this query
-
-        // Recalculate the entire sum from scratch
-        totalSum = 0;
-        dfs(1, -1); // Start DFS from node 1 (or any node)
-
-        // Print the answer for THIS query (and fix the double-counting)
-        cout << totalSum / 2 << "\n";
-    }
+    
 }
 
 signed main() {
---
+++ Swami Saranam/Codeforces/temp.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>>queries={{0,1},{2,2},{0,3}};
+    return 0;
+}
