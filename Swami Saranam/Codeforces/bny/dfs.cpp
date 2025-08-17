d26302e69a6c7ee253c0a52f4282b3b5c2a3d67c
adofm <karthik.maniam008@gmail.com>
2025-08-17 19:09:52 +0530

    added more

---
+++ Swami Saranam/Codeforces/bny/dfs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+void solve(){
+    vector<vector<int>>adj;
+    int n;
+    cin>>n;
+    
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
