6f53978f7c22837881169af426a2231acdf54b2f
adofm <karthik.maniam008@gmail.com>
2025-09-07 09:29:42 +0530

    added more code

--- Swami Saranam/Codeforces/2132D.cpp
+++ Swami Saranam/Codeforces/2132D.cpp
@@ -1,3 +1,4 @@
+// karthik solving bois
 #include <bits/stdc++.h>
 using namespace std;
 #define fast()                        \
@@ -14,29 +15,28 @@ using namespace std;
 #define MOD (1000000007)
 #define INF 1000000000000000000LL // Infinity for ll
 #define mp make_pair
-#define nline '\n'
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
 #define ff first
 #define ss second
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-const int inf = 1e18;
-const int mod = 1e9 + 7;
-const int NUM = 1e6 + 5; 
-const int N = 1e7 + 5;  
+#define sz(x) (ll)(x).size()
+const ll inf = 1e18;
+const ll mod = 1e9 + 7;
+const ll NUM = 1e6 + 5; 
+const ll N = 1e7 + 5;  
 #define DEBUG(x) cerr << #x << ": " << x << '\n'
-vector<int> fact, invfact, power, sieve, lp, primes;
+vector<ll> fact, invfact, power, sieve, lp, primes;
 vector<vector<pair<ll,ll>>> adj;
 vector<vector<ll>> adj_unweighted;
 vector<ll> dist;
 vector<bool> visited;
 
-int mod_pow(int a, int b, int m = mod) {
-    int res = 1;
+ll mod_pow(ll a, ll b, ll m = mod) {
+    ll res = 1;
     a %= m;
     while (b) {
         if (b & 1) res = (res * a) % m;
@@ -45,146 +45,90 @@ int mod_pow(int a, int b, int m = mod) {
     }
     return res;
 }
-int mod_inv(int a, int m = mod) {
+ll mod_inv(ll a, ll m = mod) {
     return mod_pow(a, m - 2, m);
 }
-int mod_div(int a, int b, int m = mod) {
+ll mod_div(ll a, ll b, ll m = mod) {
     return (a % m * mod_inv(b, m)) % m;
 }
-void init_factorials(int max_n = NUM) {
+void init_factorials(ll max_n = NUM) {
     fact.resize(max_n);
     invfact.resize(max_n);
     fact[0] = invfact[0] = 1;
-    for (int i = 1; i < max_n; i++)
+    for (ll i = 1; i < max_n; i++)
         fact[i] = (fact[i - 1] * i) % mod;
     invfact[max_n - 1] = mod_inv(fact[max_n - 1]);
-    for (int i = max_n - 2; i >= 1; i--)
+    for (ll i = max_n - 2; i >= 1; i--)
         invfact[i] = (invfact[i + 1] * (i + 1)) % mod;
 }
-int combination(int n, int k) {
+ll combination(ll n, ll k) {
     if (k > n || k < 0) return 0;
     return fact[n] * invfact[k] % mod * invfact[n - k] % mod;
 }
-void init_powers(int x, int max_n = NUM) {
+void init_powers(ll x, ll max_n = NUM) {
     power.resize(max_n);
     power[0] = 1;
-    for (int i = 1; i < max_n; i++)
+    for (ll i = 1; i < max_n; i++)
         power[i] = (power[i - 1] * x) % mod;
 }
-void calc_sieve(int max_n = NUM) {
+void calc_sieve(ll max_n = NUM) {
     sieve.assign(max_n + 1, 0);
-    for (int i = 2; i <= max_n; ++i) {
+    for (ll i = 2; i <= max_n; ++i) {
         if (!sieve[i]) {
-            for (int j = i; j <= max_n; j += i)
+            for (ll j = i; j <= max_n; j += i)
                 if (!sieve[j]) sieve[j] = i;
         }
     }
 }
-void linear_sieve(int max_n = N) {
+void linear_sieve(ll max_n = N) {
     lp.assign(max_n + 1, 0);
-    for (int i = 2; i <= max_n; ++i) {
+    for (ll i = 2; i <= max_n; ++i) {
         if (lp[i] == 0) {
             lp[i] = i;
             primes.pb(i);
         }
-        for (int j = 0; j < sz(primes) && primes[j] <= lp[i] && i * primes[j] <= max_n; ++j)
+        for (ll j = 0; j < sz(primes) && primes[j] <= lp[i] && i * primes[j] <= max_n; ++j)
             lp[i * primes[j]] = primes[j];
     }
 }
 
-int gcd(int a, int b) {
+ll gcd(ll a, ll b) {
     return b ? gcd(b, a % b) : a;
 }
-int lcm(int a, int b) {
+ll lcm(ll a, ll b) {
     return (a / gcd(a, b)) * b;
 }
-
-void init_graph_weighted(ll n) {
-    adj.assign(n + 1, {});
-    dist.assign(n + 1, INF);
-    visited.assign(n + 1, false);
-}
-
-void dfs(ll u) {
-    visited[u] = true;
-    for (ll v : adj_unweighted[u]) {
-        if (!visited[v]) dfs(v);
-    }
-}
-
-void bfs(ll start) {
-    queue<ll> q;
-    dist.assign(sz(adj_unweighted), INF);
-    visited.assign(sz(adj_unweighted), false);
-    dist[start] = 0;
-    visited[start] = true;
-    q.push(start);
-    while (!q.empty()) {
-        ll u = q.front();
-        q.pop();
-        for (ll v : adj_unweighted[u]) {
-            if (!visited[v]) {
-                visited[v] = true;
-                dist[v] = dist[u] + 1;
-                q.push(v);
-            }
-        }
-    }
-}
-
-void dijkstra(ll start) {
-    dist.assign(sz(adj), INF);
-    dist[start] = 0;
-    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
-    pq.push({0, start});
-    while (!pq.empty()) {
-        auto [d, u] = pq.top();
-        pq.pop();
-        if (d != dist[u]) continue;
-        for (auto [v, w] : adj[u]) {
-            if (dist[u] + w < dist[v]) {
-                dist[v] = dist[u] + w;
-                pq.push({dist[v], v});
-            }
-        }
-    }
-}
-
-int sumOfDigitsFrom1ToNUtil(int n, vector<int> &a) {
-    if (n < 10)
-        return (n * (n + 1) / 2);
-
-    int d = (int)(log10(n));
-    int p = (int)(ceil(pow(10, d)));
-    int msd = n / p;
-
-    return (msd * a[d] + (msd * (msd - 1) / 2) * p 
-            + msd * (1 + n % p) + sumOfDigitsFrom1ToNUtil(n % p, a));
-}
-
-// Function to computer sum of digits in
-// numbers from 1 to n
-int sumOfDigits(int n) {
-    int d = max(((int)(log10(n))), 1);
-    vector<int> a(d + 1);
-    a[0] = 0;
-    a[1] = 45;
-
-    for (int i = 2; i <= d; i++)
-        a[i] = a[i - 1] * 10 + 45 * (int)(ceil(pow(10, i - 1)));
-
-    return sumOfDigitsFrom1ToNUtil(n, a);
-}
-
-void solve(){
+// thalivare check and write in your note first 
+// dont bullshit 
+// think propelry 
+// solve fast 
+// check for cin>>t if that needed or just one testcase 
+void solve() {
     ll k;
-    cin>>
-    cout << sumOfDigits(n);
+    cin >> k;
+    ll cur = 9, len = 1;
+    while (k - cur * len > 0) {
+        k -= cur * len;
+        cur *= 10;
+        len++;
+    }
+    string s = to_string(cur / 9 + (k - 1) / len);
+    ll ans = 0;
+    for (int i = 0; i < (k - 1) % len + 1; i++)
+        ans += s[i] - '0';
+    ll pr_s = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int curd = s[i] - '0';
+        if (curd)
+            ans += curd * (len - 1) * cur / 2 + curd * (2 * pr_s + curd - 1) / 2 * cur / 9;
+        cur /= 10, len--;
+        pr_s += curd;
+    }
+    cout << ans << '\n';
 }
-
 signed main() {
     fast();
-    int t = 1;
+    ll t = 1;
     cin >> t;
     while (t--) solve();
     return 0;
