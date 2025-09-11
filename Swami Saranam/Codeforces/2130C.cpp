0f23fce7a19693126881c02063b26b083e31ad63
adofm <karthik.maniam008@gmail.com>
2025-09-11 21:27:54 +0530

    sovled more

--- Swami Saranam/Codeforces/2130C.cpp
+++ Swami Saranam/Codeforces/2130C.cpp
@@ -1,3 +1,4 @@
+// karthik solving bois
 #include <bits/stdc++.h>
 using namespace std;
 #define fast()                        \
@@ -14,25 +15,28 @@ using namespace std;
 #define MOD (1000000007)
 #define INF 1000000000000000000LL // Infinity for ll
 #define mp make_pair
-#define nline '\n'
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
 #define ff first
 #define ss second
 #define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-const int inf = 1e18;
-const int mod = 1e9 + 7;
-const int NUM = 1e6 + 5; 
-const int N = 1e7 + 5;  
+#define sz(x) (ll)(x).size()
+const ll inf = 1e18;
+const ll mod = 1e9 + 7;
+const ll NUM = 1e6 + 5; 
+const ll N = 1e7 + 5;  
 #define DEBUG(x) cerr << #x << ": " << x << '\n'
-vector<int> fact, invfact, power, sieve, lp, primes;
+vector<ll> fact, invfact, power, sieve, lp, primes;
+vector<vector<pair<ll,ll>>> adj;
+vector<vector<ll>> adj_unweighted;
+vector<ll> dist;
+vector<bool> visited;
 
-int mod_pow(int a, int b, int m = mod) {
-    int res = 1;
+ll mod_pow(ll a, ll b, ll m = mod) {
+    ll res = 1;
     a %= m;
     while (b) {
         if (b & 1) res = (res * a) % m;
@@ -41,85 +45,92 @@ int mod_pow(int a, int b, int m = mod) {
     }
     return res;
 }
-int mod_inv(int a, int m = mod) {
+ll mod_inv(ll a, ll m = mod) {
     return mod_pow(a, m - 2, m);
 }
-int mod_div(int a, int b, int m = mod) {
+ll mod_div(ll a, ll b, ll m = mod) {
     return (a % m * mod_inv(b, m)) % m;
 }
-void init_factorials(int max_n = NUM) {
+void init_factorials(ll max_n = NUM) {
     fact.resize(max_n);
     invfact.resize(max_n);
     fact[0] = invfact[0] = 1;
-    for (int i = 1; i < max_n; i++)
+    for (ll i = 1; i < max_n; i++)
         fact[i] = (fact[i - 1] * i) % mod;
     invfact[max_n - 1] = mod_inv(fact[max_n - 1]);
-    for (int i = max_n - 2; i >= 1; i--)
+    for (ll i = max_n - 2; i >= 1; i--)
         invfact[i] = (invfact[i + 1] * (i + 1)) % mod;
 }
-int combination(int n, int k) {
+ll combination(ll n, ll k) {
     if (k > n || k < 0) return 0;
     return fact[n] * invfact[k] % mod * invfact[n - k] % mod;
 }
-void init_powers(int x, int max_n = NUM) {
+void init_powers(ll x, ll max_n = NUM) {
     power.resize(max_n);
     power[0] = 1;
-    for (int i = 1; i < max_n; i++)
+    for (ll i = 1; i < max_n; i++)
         power[i] = (power[i - 1] * x) % mod;
 }
-void calc_sieve(int max_n = NUM) {
+void calc_sieve(ll max_n = NUM) {
     sieve.assign(max_n + 1, 0);
-    for (int i = 2; i <= max_n; ++i) {
+    for (ll i = 2; i <= max_n; ++i) {
         if (!sieve[i]) {
-            for (int j = i; j <= max_n; j += i)
+            for (ll j = i; j <= max_n; j += i)
                 if (!sieve[j]) sieve[j] = i;
         }
     }
 }
-void linear_sieve(int max_n = N) {
+void linear_sieve(ll max_n = N) {
     lp.assign(max_n + 1, 0);
-    for (int i = 2; i <= max_n; ++i) {
+    for (ll i = 2; i <= max_n; ++i) {
         if (lp[i] == 0) {
             lp[i] = i;
             primes.pb(i);
         }
-        for (int j = 0; j < sz(primes) && primes[j] <= lp[i] && i * primes[j] <= max_n; ++j)
+        for (ll j = 0; j < sz(primes) && primes[j] <= lp[i] && i * primes[j] <= max_n; ++j)
             lp[i * primes[j]] = primes[j];
     }
 }
 
-int gcd(int a, int b) {
+ll gcd(ll a, ll b) {
     return b ? gcd(b, a % b) : a;
 }
-int lcm(int a, int b) {
+ll lcm(ll a, ll b) {
     return (a / gcd(a, b)) * b;
 }
-
+// thalivare check and write in your note first 
+// dont bullshit 
+// think propelry 
+// solve fast 
+// check for cin>>t if that needed or just one testcase 
 void solve() {
     ll n;
     cin>>n;
-    vector<ll>x(n);
-    vector<ll>y(n);
-    for(ll i=0;i<n;i++){
+    vector<int>x(n);
+    vector<int>y(n);
+    for(int i=0;i<n;i++){
         cin>>x[i]>>y[i];
     }
-    vector<ll>flag(n,1);
-    vector<ll>ans;
-    for(ll i=0;i<n;i++){
-        for(ll j=0;j<n;j++){
+    vector<int>flag(n,1);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             if(i==j) continue;
             if(x[j]<=x[i] && y[j]>=y[i]) flag[i]=0;
         }
-        if(flag[i]) ans.push_back(i+1);
+    }
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        if(flag[i]==1) ans.push_back(i+1);
     }
     cout<<ans.size()<<endl;
-    for(ll i=0;i<ans.size();i++) cout<<ans[i]<<" ";
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
     cout<<endl;
 }
-
 signed main() {
     fast();
-    int t = 1;
+    ll t = 1;
     cin >> t;
     while (t--) solve();
     return 0;
